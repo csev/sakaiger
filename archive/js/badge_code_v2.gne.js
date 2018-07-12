eb61499aab61d5b9ed000e2b3600b1c4d33a8db9
Charles Severance <csev@umich.edu>
2018-07-12 07:35:21 -0400

    first commit

---
+++ archive/js/badge_code_v2.gne.js
@@ -0,0 +1,37 @@
+var b_txt = '';
+
+// write the badge
+	
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image1"><a href="http://www.flickr.com/photos/gforsythe/8141702262/"><img src="http://farm9.staticflickr.com/8331/8141702262_5482d8179f_s.jpg" alt="A photo on Flickr" title="My costume: Open Source #Sakai Tiger (Sakaiger) cc @DrChuck" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image2"><a href="http://www.flickr.com/photos/ixmati/6723326641/"><img src="http://farm8.staticflickr.com/7149/6723326641_0f06f5d5e1_s.jpg" alt="A photo on Flickr" title="Sakaiger Eraser" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image3"><a href="http://www.flickr.com/photos/mathplourde/4879807723/"><img src="http://farm5.staticflickr.com/4074/4879807723_da8b3ee81b_s.jpg" alt="A photo on Flickr" title="Rock On, Sakaiger! In 3D!" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image4"><a href="http://www.flickr.com/photos/stephen_marquard/4718580487/"><img src="http://farm5.staticflickr.com/4035/4718580487_6b9fc76992_s.jpg" alt="A photo on Flickr" title="Orange Sakaiger" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image5"><a href="http://www.flickr.com/photos/ixmati/4706932066/"><img src="http://farm5.staticflickr.com/4027/4706932066_84095348eb_s.jpg" alt="A photo on Flickr" title="Sakai Fellowship Sakaiger" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image6"><a href="http://www.flickr.com/photos/lances/4473867708/"><img src="http://farm5.staticflickr.com/4035/4473867708_0b9a81da5e_s.jpg" alt="A photo on Flickr" title="ja-sakaiger" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image7"><a href="http://www.flickr.com/photos/lances/4473090501/"><img src="http://farm3.staticflickr.com/2768/4473090501_7ed686baab_s.jpg" alt="A photo on Flickr" title="ja-sakaiger 2" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image8"><a href="http://www.flickr.com/photos/stephen_marquard/4289974615/"><img src="http://farm5.staticflickr.com/4065/4289974615_f284dd52c1_s.jpg" alt="A photo on Flickr" title="Sarah meets Sakaigers" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image9"><a href="http://www.flickr.com/photos/mathplourde/3727184405/"><img src="http://farm4.staticflickr.com/3219/3727184405_d3272f2e4e_s.jpg" alt="A photo on Flickr" title="Pic of the Profile2 Tool" height="75" width="75"></a></td>';
+	
+		 	 		
+	b_txt+= '<td align="center" valign="center" style="padding:0" class="flickr_badge_image" id="flickr_badge_image10"><a href="http://www.flickr.com/photos/mathplourde/3727184965/"><img src="http://farm4.staticflickr.com/3621/3727184965_3c1afba73c_s.jpg" alt="A photo on Flickr" title="Sakaiger!" height="75" width="75"></a></td>';
+
+
+document.write(b_txt);
---
+++ sakaiger1/old/index.php
@@ -0,0 +1,92 @@
+<head>
+<title>Sakaiger Home Page</title>
+</head>
+<body>
+<img src=sakaiger-150.png align=right>
+<h3>The Legend of the Sakaiger</h3>
+(Under construction)
+<p>
+The Sakaiger is a happy dancing creature.  It is the logo of the hip, cool, underground, karaoke singing, 
+silly elements of the Sakai Community (that is pretty much everyone).  The Sakaiger is very much part of the lore
+of the Sakai Quality Assurance effort.
+<p>
+<a href=http://bugs.sakaiproject.org/confluence/display/QA/Home>Sakai QA Page</a>
+<br clear=all>
+<img src=sakaiger-megan-from-ian.jpg align=right width=150>
+<h3>Sakaiger Facts</h3>
+<ul>
+<li>Plush Sakaigers are in very limited supply given to those with significant 
+contribution/leadership in Sakai QA Activity
+<li>The first Sakaiger is larger than the rest and was given to Megan May - 
+the Sakai QA director.
+<li>Sakaigers were first sighted in Amsterdam on June 13, 2007 at the Sakai Conference.
+<li>
+Follow the worldwide travels of the Sakaiger
+<a href=http://www.flickr.com/photos/tags/sakaiger/>
+via Flickr</a>.  Please upload your Sakaiger photos and tag them with the "sakaiger" tag.
+<li>Andrew Thornton of CARET / Cambridge designed the original Sakaiger image.
+<li>Each stuffed Sakaiger is hand sewn in Perry, MI
+(<a href=http://maps.google.com/maps?f=q&hl=en&q=n+church+st+vernon+mi&sll=42.939397,-84.032364&sspn=0.018693,0.025706&ie=UTF8&om=1&ll=42.940214,-84.030819&spn=0.037386,0.051413&z=14&iwloc=addr target=_new>Map</a>).  
+The stuffed Sakaiger is based on a Simplicity 3779 pattern for a stuffed rabbit (www.simplicity.com) - the pattern was adapted to make the Sakaiger more tiger-like.
+<li>It is not possible to purchase a Sakaiger at this time - they are only available as gifts.
+<li>A relative of the Sakaiger - the 
+<a href=http://www.weebls-stuff.com/toons/kenya/>
+Kenya Tiger</a> from 
+<a href=http://www.weebls-stuff.com/>Weebls</a>.  Please visit the site, view the
+video and buy some of the totally sweet Weebl Stuff.
+</ul>
+<p>
+<!-- Start of Flickr Badge -->
+<style type="text/css">
+#flickr_badge_source_txt {padding:0; font: 11px Arial, Helvetica, Sans serif; color:#666666;}
+#flickr_badge_icon {display:block !important; margin:0 !important; border: 1px solid rgb(0, 0, 0) !important;}
+#flickr_icon_td {padding:0 5px 0 0 !important;}
+.flickr_badge_image {text-align:center !important;}
+.flickr_badge_image img {border: 1px solid black !important;}
+#flickr_www {display:block; text-align:left; padding:0 10px 0 10px !important; font: 11px Arial, Helvetica, Sans serif !important; color:#3993ff !important;}
+#flickr_badge_uber_wrapper a:hover,
+#flickr_badge_uber_wrapper a:link,
+#flickr_badge_uber_wrapper a:active,
+#flickr_badge_uber_wrapper a:visited {text-decoration:none !important; background:inherit !important;color:#3993ff;}
+#flickr_badge_wrapper {background-color:#ffffff;border: solid 1px #000000}
+#flickr_badge_source {padding:0 !important; font: 11px Arial, Helvetica, Sans serif !important; color:#666666 !important;}
+</style>
+<table id="flickr_badge_uber_wrapper" cellpadding="0" cellspacing="10" border="0"><tr><td><a href="http://www.flickr.com" id="flickr_www">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a><table cellpadding="0" cellspacing="10" border="0" id="flickr_badge_wrapper">
+<tr>
+<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&count=5&display=latest&size=t&layout=h&source=all_tag&tag=sakaiger&user=38353709%40N00"></script>
+<td id="flickr_badge_source" valign="center" align="center">
+<table cellpadding="0" cellspacing="0" border="0"><tr>
+<td id="flickr_badge_source_txt">More <a href="http://www.flickr.com/photos/tags/sakaiger/">Flickr photos tagged with sakaiger</a></td>
+</tr></table>
+</td>
+</tr>
+</table>
+</td></tr></table>
+<!-- End of Flickr Badge -->
+<p>
+If you have some comments or info to contribute to this web site let Dr. Chuck know.
+<br clear=all>
+<h3>Notes</h3>
+From Harriet: The original concept of a Sakaiger was mainly a silly pun thought up by
+those crazy folks at CARET, Cambridge. We have intermittently talked about
+other Sakai related animals (Sakainocerous, for example), but none of them
+really caught on as the Sakaiger did. We wanted to have a real
+implementation of a Sakaiger, and Andy Thornton drew one, which ended up
+looking rather like the tiger from the Weebls cartoon 'Kenya'.  
+Andy also bought a Weebl tiger and lion (and t-shirt) for the office.  We
+customised our Weebl tiger a bit with a Sakai badge in its ear... The real
+life Sakaigers as realised by Chuck don't look much like the Weebl one
+(except they're clearly both meant to be tigers) - the Sakaiger is much
+taller and thinner and is smily.
+<p>
+<font size="-1">
+The Sakaiger stuffed animal and this web site are Copyright Creative Commons Attribution 2.5 - Charles Severance.
+You are welcome and encouraged to make derivative works of the stuffed Sakaiger and evolve the 
+species through artistic improvement.  Attribution is sufficient is you reference www.sakaiger.com
+if you display your artwork on your own web site.  There is no need to put any attribtion on the 
+derivative works themselves unless you want to - this is after all - art.
+<p>
+Sorry about the legal stuff - Sakai is an open source project and the Sakaiker needs to be an open
+source mascot.  :)
+</font>
+</body>
